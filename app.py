243c997c85c3659fb650fea09810c4ee43c255a7
SriramNamratha <namrathasriram28@gmail.com>
2024-05-17 01:16:57 +0530

    database storing updated

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 app = Flask(__name__)
@@ -17,15 +17,33 @@ class Todo(db.Model):
     
 with app.app_context():
     db.create_all()
-@app.route("/")
+
+@app.route("/",methods = ['GET','POST'])
 def hello_world():
-    todo = Todo(title = "First Todo" , desc = "Start investing in stocks")
-    db.session.add(todo)
-    db.session.commit()
-    return render_template("index.html")
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo(title = title , desc = desc)
+        db.session.add(todo)
+        db.session.commit()
+    all_todos = Todo.query.all()
+    return render_template("index.html",all_todos = all_todos)
 
 @app.route("/show")
 def products():
+     all_todos = Todo.query.all()
+     print(all_todos)
+     return "this is products page"
+
+@app.route("/delete/<int:sno>")
+def delete(sno):
+     todo = Todo.query.filter_by(sno=sno).first()
+     db.session.delete(todo)
+     db.session.commit()
+     return redirect("/")
+
+@app.route("/update/<int:sno>")
+def update():
      all_todos = Todo.query.all()
      print(all_todos)
      return "this is products page"
